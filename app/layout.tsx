d0b6872185e6b632b85b020152f582f332b35018
Gabrielleholand <gabrielleholanda7@gmail.com>
2026-05-24 21:05:33 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '5431969973535179');
+            fbq('init', '4509292342725804');
             fbq('track', 'PageView');
           `}
         </Script>
